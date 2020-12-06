02961264d24d62aebfb616dae3f82077ea46067c
Javier Zapata <javierzapata82@gmail.com>
2020-12-06 16:48:44 +0100

    Refactor SVG gradient defs to its own file

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Router from 'svelte-spa-router';
+  import Gradients from "./assets/images/gradients.svg";
 	import routes from './routes';
 </script>
 
 <main>
-	<Router {routes} restoreScrollState={true} />
+  <Router {routes} restoreScrollState={true} />
+  <Gradients />
 </main>
